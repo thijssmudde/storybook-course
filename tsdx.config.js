b1ce6375227df8fa5cf7eceb34699489e819187d
Thijs Smudde <thijssmudde@kateinnovations.com>
2021-10-24 19:23:34 +0200

    save

--- tsdx.config.js
+++ tsdx.config.js
@@ -2,11 +2,6 @@ const postcss = require("rollup-plugin-postcss");
 
 module.exports = {
   rollup(config, options) {
-    // Include external dependencies: https://github.com/jaredpalmer/tsdx/issues/179
-    if (config.output.format === "umd") {
-      delete config.external;
-    }
-
     config.plugins.push(
       postcss({
         config: {
