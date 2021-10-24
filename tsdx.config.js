9b66197849497861332de061a4e22b6c00534b25
Thijs Smudde <thijssmudde@kateinnovations.com>
2021-10-24 18:10:02 +0200

    include external dependencies

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,18 +1,23 @@
-const postcss = require('rollup-plugin-postcss');
+const postcss = require("rollup-plugin-postcss");
 
 module.exports = {
   rollup(config, options) {
+    // Include external dependencies: https://github.com/jaredpalmer/tsdx/issues/179
+    if (config.output.format === "umd") {
+      delete config.external;
+    }
+
     config.plugins.push(
       postcss({
         config: {
-          path: './postcss.config.js',
+          path: "./postcss.config.js",
         },
-        extensions: ['.css'],
+        extensions: [".css"],
         minimize: true,
         inject: {
-          insertAt: 'top',
+          insertAt: "top",
         },
-      })
+      }),
     );
     return config;
   },
